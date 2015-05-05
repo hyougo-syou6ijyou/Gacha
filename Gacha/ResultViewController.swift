9528d6fcf6e3012330b0aafa57eff10c05d137ce
木嶋将登 <harima1999.hyougo.maruhana@gmail.com>
2015-05-05 21:39:40 +0900

    first commit

---
+++ Gacha/ResultViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ResultViewController.swift
+//  Gacha
+//
+//  Created by 木嶋将登 on 2015/05/01.
+//  Copyright (c) 2015年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    var number:Int!
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var monsterImageView: UIImageView!
+    @IBOutlet var label:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        number = Int(arc4random_uniform(10))
+
+        // Do any additional setup after loading the view.
+        NSLog("渡された値は...%dです", number)
+        
+        if number == 9{
+            monsterImageView.image = UIImage(named: "monster010")
+            backgroundImageView.image = UIImage(named: "bg_gold")
+            label.text = "激レアモンスター"
+        }else if number > 7{
+            monsterImageView.image = UIImage(named: "monster006")
+            backgroundImageView.image = UIImage(named: "bg_red")
+            label.text = "レアモンスター"
+        }else{
+            monsterImageView.image = UIImage(named: "monster003")
+            backgroundImageView.image = UIImage(named: "bg_blue")
+            label.text = "普通のモンスター"
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func back(){
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Gacha/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Gacha
+//
+//  Created by 木嶋将登 on 2015/05/01.
+//  Copyright (c) 2015年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var number:Int!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func getRondomNumber(){
+        number = Int(arc4random_uniform(10))
+        NSLog("発生した乱数は...%dです", number)
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var resultViewController = segue.destinationViewController as!
+            ResultViewController
+        
+        resultViewController.number = self.number
+    }
+
+}
+
